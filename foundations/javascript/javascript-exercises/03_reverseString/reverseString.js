a77aca0f27013f56182c1016fed793225162bd23
Will Wallace <willwallace212@gmail.com>
2023-12-31 13:30:41 +1100

    Finished JS exercise 3.

--- foundations/javascript/javascript-exercises/03_reverseString/reverseString.js
+++ foundations/javascript/javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = [];
+    let str = '';
+    for(let i = 0; i < string.length; i++) {
+        arr.push(string[i]);
+    }
 
+    for(let i = arr.length-1; i >= 0; i--) {
+        str += arr[i];
+    }
+    return str;
 };
 
 // Do not edit below this line
