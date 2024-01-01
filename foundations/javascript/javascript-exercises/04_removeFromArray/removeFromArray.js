506509277a47e95b586b490b8c20e5927a668021
Will Wallace <willwallace212@gmail.com>
2024-01-01 14:32:41 +1100

    Finished JS exercise 4.

--- foundations/javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ foundations/javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let arr = array;
+    
+    for (let i = 0; i < args.length; i++){ 
+        if(arr.indexOf(args[i]) !== -1) {
+            let index = arr.indexOf(args[i]);
+            arr.splice(index, 1);
+        }
+    }
 
+    return arr;
 };
 
 // Do not edit below this line
