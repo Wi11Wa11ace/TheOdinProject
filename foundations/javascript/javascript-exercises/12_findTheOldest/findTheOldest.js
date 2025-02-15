60eed8feb18fe035b9a54c365b88b1702a1d05f8
Will Wallace <willwallace212@gmail.com>
2025-02-15 15:23:17 +1100

    Finished JS exercise 12.

--- foundations/javascript/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ foundations/javascript/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = calcAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = calcAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
 
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+    });
+};
+
+function calcAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
