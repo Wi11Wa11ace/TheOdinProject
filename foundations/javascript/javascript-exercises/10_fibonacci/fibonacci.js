a221f6af397877f4f8e93127c04680450e28002d
Will Wallace <willwallace212@gmail.com>
2025-02-15 15:13:25 +1100

    Finished JS exercise 10.

--- foundations/javascript/javascript-exercises/10_fibonacci/fibonacci.js
+++ foundations/javascript/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    num = parseInt(num);
+
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num == 1 || num == 2) return 1;
+
+    let first = 1, second = 1, current;
+    
+    for(let i = 2; i < num; i++) {
+        current = first + second;
+        first = second;
+        second = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
