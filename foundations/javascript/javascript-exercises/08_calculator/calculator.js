370abb61c745195d2719444bbfbb98ca650f30d5
Will Wallace <willwallace212@gmail.com>
2025-02-15 14:51:57 +1100

    Finished JS exercise 8.

--- foundations/javascript/javascript-exercises/08_calculator/calculator.js
+++ foundations/javascript/javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a ,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentNum) => {
+    return total + currentNum;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentNum) => {
+    return total * currentNum;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) {return 1;};
+
+	let newArray = [];
+  while (a > 0) {
+    newArray.push(a);
+    a--;
+  };
+
+  return newArray.reduce((total, currentNum) => {
+    return total * currentNum;
+  });
 };
 
 // Do not edit below this line
