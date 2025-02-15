b840dbc7bf7d22049cdf37f11a2c8289b58e628a
Will Wallace <willwallace212@gmail.com>
2025-02-15 15:02:57 +1100

    Finished JS exercise 9.

--- foundations/javascript/javascript-exercises/09_palindromes/palindromes.js
+++ foundations/javascript/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const lowerCaseWord = word.toLowerCase();
+    const wordWithoutPunctuation = lowerCaseWord.replace(/[^a-z0-9]/g, "");
 
+    const wordBackwards = wordWithoutPunctuation.split("").reverse().join("");
+
+    return wordBackwards == wordWithoutPunctuation;
 };
 
 // Do not edit below this line
