b94d48d4473e52b095d5b191f9f8f64cb4dd8998
Will Wallace <willwallace212@gmail.com>
2025-06-11 08:57:08 +1000

    Uploaded JS object and object contructors script.js file.

---
+++ javascript/objects-and-object-containers/script.js
@@ -0,0 +1,15 @@
+function Book(title, author, pages, read) {
+    if(!new.target) {
+        throw Error("Book must be called with new");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
+    }
+}
+
